54030e1609801eced7f50ea2cc6f3b7543c43472
Franklin Souza <ffrancadesouza@gmail.com>
2023-02-27 15:25:49 -0300

    Adicionando validações de preenchimento de campos nome, email, senha e data de nascimento

--- scripts/validacao.js
+++ scripts/validacao.js
@@ -4,10 +4,37 @@ export function valida(input) {
     if(validadores[tipoDeInput]) {
         validadores[tipoDeInput](input)
     }
+
+    if(input.validity.valid) {
+        input.parentElement.classList.remove('input-container--invalido')
+        input.parentElement.querySelector('.input-mensagem-erro').innerHTML = ' '
+
+    } else {
+        input.parentElement.classList.add('input-container--invalido')
+        input.parentElement.querySelector('.input-mensagem-erro').innerHTML = mostraMensagemDeErro(tipoDeInput, input)
+    }
 }
 
 const validadores = {
     dataNascimento:input => validaDataNascimento(input)
+}
+
+const tiposDeErro = [
+    'valueMissing',
+    'typeMismatch',
+    'patternMismatch',
+    'customError',
+]
+
+function mostraMensagemDeErro(tipoDeInput, input) {
+    let mensagem = ''
+    
+    tiposDeErro.forEach(erro => {
+        if(input.validity[erro]) {
+            mensagem = mensagensDeErro[tipoDeInput][erro]
+       }})
+
+    return mensagem
 }
 
 function validaDataNascimento(input) {
@@ -26,4 +53,22 @@ function maiorQue18(data) {
     const dataMais18 = new Date(data.getUTCFullYear() + 18, data.getUTCMonth(), data.getUTCDate())
 
     return dataMais18 <= dataAtual
+}
+
+const mensagensDeErro = {
+    nome: {
+        valueMissing: 'O campo de nome não pode estar vazio.',        
+    },
+    email:{
+        valueMissing: 'O campo de email não pode estar vazio.',
+        typeMismatch: 'O email digitado não é válido.'
+    },
+    senha:{
+        valueMissing: 'O campo de senha não pode estar vazio.',
+        patternMismatch: 'A senha deve conter entre 6 e 12 caracteres, conter pelo menos uma letra maiúscula, uma letra minúscula, um número e um caractere especial.'
+    },
+    dataNascimento:{
+        valueMissing: 'O campo de data de nascimento não pode estar vazio.',
+        customError: 'Você deve ser maior que 18 anos para se cadastrar.'
+    },
 }
